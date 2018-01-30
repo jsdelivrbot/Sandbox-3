a04adf9a6f96bfdf2fb303e1a3293bce780e09f5
Paul Terry <paul.terry84@gmail.com>
2018-01-30 20:22:06 +0000

    added hooks for the tests and our first assert

--- mongodb/udemy-complete-developers-guide-to-mongodb/02-a-test-driven-experience/users/test/create_test.js
+++ mongodb/udemy-complete-developers-guide-to-mongodb/02-a-test-driven-experience/users/test/create_test.js
@@ -10,11 +10,19 @@ describe('Creating records', () => {
   // each test is created in an 'if' function
   // the string 'saves a users' explains what the test is doing
   // inside each 'it' function we will have an assertion to check whether values are as we expect
-  it('saves a user', () => {
+  it('saves a user', (done) => {
     // create the user
     const joe = new User({ name: 'Joe' });
 
     // save the user
-    joe.save();
+    joe.save()
+      .then(() => {
+        // has Joe been saved successfully?
+        // the isNew boolean property is specific to mocha.
+        // it will be true when the model instance 'joe' is waiting to be saved by mocha.
+        // once it is saved in the database, the isNew property becomes false.  So here if it is false, then it is saved.
+        assert(!joe.isNew);
+        done();
+      });
   });
 });
--- mongodb/udemy-complete-developers-guide-to-mongodb/02-a-test-driven-experience/users/test/test_helper.js
+++ mongodb/udemy-complete-developers-guide-to-mongodb/02-a-test-driven-experience/users/test/test_helper.js
@@ -3,17 +3,32 @@
 // require mongoose module
 const mongoose = require('mongoose');
 
+// when mongoose needs to use a promise, we want to use ES6 promises.
+mongoose.Promise = global.Promise;
+
 // connect to the local mongodb instance
 // we have to tell mongoose exactly where our database is 'mongodb://localhost/'
 // and which database to use							  'users_test'
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-  // do this once i.e. once the connection is open
-  .once('open', () => {
-    console.log('Connection made. Good to go!')
-  })
-  // do this each time - when mongoose emits an error
-  .on('error', (e) => {
-    console.warn('Error: ', e);
-  });
+// before is a hook which will run before once, before anything else happens - here we want to make sure we are connected to mongoose before we run our tests.
+before((done) => {
+  mongoose.connect('mongodb://localhost/users_test');
+  mongoose.connection
+    // do this once i.e. once the connection is open
+    .once('open', () => {
+      done();
+    })
+    // do this each time - when mongoose emits an error
+    .on('error', (e) => {
+      console.warn('Error: ', e);
+    });
+});
 
+// a hook is a function that gets executed before any test gets executed inside our test suite
+
+// beforeEach is a hook it will run before each test
+// each function in mocha can take a done callback function, this essentially says to mocha you can carry on once this done callback function is called.
+beforeEach((done) => {
+  mongoose.connection.collections.users.drop(() => {
+    done();
+  });
+});
