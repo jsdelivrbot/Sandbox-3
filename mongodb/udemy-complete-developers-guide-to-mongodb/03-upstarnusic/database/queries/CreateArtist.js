92db69384a436a40a48f4dbbc0bdf171d8823599
Paul Terry <paul.terry84@gmail.com>
2018-02-05 20:57:45 +0000

    added queries, for create, read, update and delete

--- mongodb/udemy-complete-developers-guide-to-mongodb/03-upstarnusic/database/queries/CreateArtist.js
+++ mongodb/udemy-complete-developers-guide-to-mongodb/03-upstarnusic/database/queries/CreateArtist.js
@@ -6,4 +6,10 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the Artist that was created
  */
 module.exports = (artistProps) => {
+  // when we create a new instance of an model, we can pass an object to it which will be used to set the data, for example,
+  // the object artistProps has the same properties (name, age, yearsActive, genre) as our artist model, so we can pass the object to it and the artist is created
+  // alternatively, we would have to create the artist by passing each of the values of the properties to create a new artist.  Eg, const artist = new Artist({ name: 'xyz' });
+  const artist = new Artist(artistProps);
+
+  return artist.save();
 };
