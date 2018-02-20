0b586f9f1fd3be6432d767695ed46ce51d07ce35
Paul Terry <paul.terry84@gmail.com>
2018-02-20 10:51:19 +0000

    added error handling middleware when the creation of a driver fails

--- mongodb/udemy-complete-developers-guide-to-mongodb/04-muber/app.js
+++ mongodb/udemy-complete-developers-guide-to-mongodb/04-muber/app.js
@@ -15,4 +15,10 @@ app.use(bodyParser.json());
 // routes() is a function (created by me) to handle our routes, such as app.get('/api)
 routes(app);
 
+// this middleware is called when an error occurs in our driver_controller route
+// specifically when trying to create a driver
+app.use((err, req, res, next) => {
+  res.status(422).send({ error: err.message });
+});
+
 module.exports = app;
--- mongodb/udemy-complete-developers-guide-to-mongodb/04-muber/controllers/drivers_controller.js
+++ mongodb/udemy-complete-developers-guide-to-mongodb/04-muber/controllers/drivers_controller.js
@@ -6,12 +6,13 @@ module.exports = {
   },
 
   // create a driver
-  create(req, res) {
+  create(req, res, next) {
     // get the data from the request body
     const driverProps = req.body;
 
     // create a new driver with the data from the request
     Driver.create(driverProps)
-      .then((driver) => res.send(driver));  // then send the driver back in the response
+      .then((driver) => res.send(driver))  // then send the driver back in the response
+      .catch(next); // passing next to the .catch method will automatically call .next to move the middleware along
   }
 };
